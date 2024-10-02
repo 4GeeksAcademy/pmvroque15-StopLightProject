850befd8beff3bffa511ec5f70fab25c8a044fa5
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-10-02 02:20:09 +0000

    initialized and finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 
-
 //create your first component
-const [activeLight, setActiveLight] = useState("red")
-
 const Home = () => {
+	const [activeLight,setActiveLight]= useState("");
 	return (
-		<div>
+<div className="bg">	
+			{/* <img src="https://i.insider.com/614e3960b414c100186262d8?width=700"/> */}
 			<div id="trafficTop"></div>
 			<div id="container">
-				<div className="red light" onClick = {() => 
-					this.setState({ clickedLight: 'red' })}></div>
-				<div className="yellow light" onClick = {() => 
-					this.setState({ clickedLight: 'yellow' })}></div>
-				<div className="green light" onClick = {() => 
-					this.setState({ clickedLight: 'green' })}></div>
+			
+				<div 
+					className={`red light ${activeLight == "red" ? " redGlow" : " "}`} 
+					onClick = {() => 
+					setActiveLight( 'red' )}
+				>
+
+				</div>
+				<div 
+					className={`yellow light ${activeLight == "yellow" ? "yellowGlow" : " "}`}
+					onClick = {() => 
+					setActiveLight( 'yellow' )}
+				>
+
+				</div>
+				<div 
+					className={`green light ${activeLight == "green" ? "greenGlow" : " "}`} 
+					onClick = {() => 
+					setActiveLight( 'green' )}
+				>
+					</div>
+				</div>
 			</div>
-		</div>
+		
 	);
 };
 
